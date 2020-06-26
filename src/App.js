4c180dc3eddb14cdf2bbeacf507774c05fa510ce
Omri Bar-Zik <omri@bar-zik.com>
2020-06-26 14:20:42 +0300

    Add: Style to countdown

--- src/App.js
+++ src/App.js
@@ -5,16 +5,23 @@ import Countdown from 'react-countdown';
 
 const renderer = ({ days, hours, minutes, seconds, completed }) => {
   return (
-    <span>
-      {days}:{hours}:{minutes}:{seconds}
-    </span>
+    <div>
+      <span className="countdown">
+        {days}  : {hours}  : {minutes}  : {seconds}
+      </span><br />
+      <span className="countdown-text">days : hours : minutes : seconds</span>
+    </div>
   );
 };
 
+const tick = (event) => {
+  //console.log(event);
+}
+
 function App() {
   return (
-    <div>
-      <Countdown date={new Date(2020,5,28,10)} renderer={renderer} /> 
+    <div className="container">
+      <Countdown date={new Date(2020, 5, 28, 10)} onTick={tick} renderer={renderer} />
     </div>
   );
 }
